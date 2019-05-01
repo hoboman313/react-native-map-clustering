d2abe88dc2c0da5ae1463b37dd73056aeaa58213
Egor <hoboman313@users.noreply.github.com>
2019-05-01 00:24:49 -0700

    fix this bs lib

--- MapView/MapWithClustering.js
+++ MapView/MapWithClustering.js
@@ -7,7 +7,7 @@ import CustomMarker from './CustomMarker';
 
 export default class MapWithClustering extends Component {
   state = {
-    currentRegion: this.props.region,
+    currentRegion: this.props.initialRegion,
     currentChildren: this.props.children,
     clusterStyle: {
       borderRadius: w(15),
@@ -17,13 +17,13 @@ export default class MapWithClustering extends Component {
       width: w(15),
       height: w(15),
       justifyContent: 'center',
-      alignItems: 'center',
+      alignItems: 'center'
     },
     clusterTextStyle: {
       fontSize: this.props.clusterTextSize,
       color: this.props.clusterTextColor,
-      fontWeight: 'bold',
-    },
+      fontWeight: 'bold'
+    }
   };
 
   componentDidMount() {
@@ -31,31 +31,38 @@ export default class MapWithClustering extends Component {
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.children != prevState.currentChildren) {
+    if (nextProps.children !== prevState.currentChildren) {
       return {
         currentChildren: nextProps.children
       };
-    } else {
-      return null
     }
+    return null;
   }
-  componentDidUpdate(prevProps, prevState) {
+
+  componentDidUpdate(prevProps) {
     if (this.props.children !== prevProps.children) {
       this.createMarkersOnMap(this.state.currentChildren);
     }
   }
 
   onRegionChangeComplete = (region) => {
-    const { latitude, latitudeDelta, longitude, longitudeDelta } = this.state.currentRegion;
+    const {
+      latitude, latitudeDelta, longitude, longitudeDelta
+    } = this.state.currentRegion;
+
     if (region.longitudeDelta <= 80) {
-      if ((Math.abs(region.latitudeDelta - latitudeDelta) > latitudeDelta / 8)
-        || (Math.abs(region.longitude - longitude) >= longitudeDelta / 5)
-        || (Math.abs(region.latitude - latitude) >= latitudeDelta / 5)) {
+      if (
+        Math.abs(region.latitudeDelta - latitudeDelta) > latitudeDelta / 8
+        || Math.abs(region.longitude - longitude) >= longitudeDelta / 5
+        || Math.abs(region.latitude - latitude) >= latitudeDelta / 5
+      ) {
         this.calculateClustersForMap(region);
       }
     }
-    if(this.props.onRegionChangeComplete)
-      this.props.onRegionChangeComplete(region)
+
+    this.setState({ currentRegion: region });
+
+    if (this.props.onRegionChangeComplete) this.props.onRegionChangeComplete(region);
   };
 
   createMarkersOnMap = () => {
@@ -70,40 +77,40 @@ export default class MapWithClustering extends Component {
             properties: { point_count: 0 },
             geometry: {
               type: 'Point',
-              coordinates: [
-                marker.props.coordinate.longitude,
-                marker.props.coordinate.latitude,
-              ],
-            },
+              coordinates: [marker.props.coordinate.longitude, marker.props.coordinate.latitude]
+            }
           });
         } else {
           otherChildren.push(marker);
         }
-      } 
+      }
     });
 
     if (!this.superCluster) {
       this.superCluster = SuperCluster({
         radius: this.props.radius,
         maxZoom: 20,
-        minZoom: 1,
+        minZoom: 1
       });
     }
     this.superCluster.load(markers);
 
-    this.setState({
-      markers,
-      otherChildren,
-    }, () => {
-      this.calculateClustersForMap();
-    });
+    this.setState(
+      {
+        markers,
+        otherChildren
+      },
+      () => {
+        this.calculateClustersForMap();
+      }
+    );
   };
 
   calculateBBox = region => [
     region.longitude - region.longitudeDelta, // westLng - min lng
     region.latitude - region.latitudeDelta, // southLat - min lat
-    region.longitude + region.longitudeDelta , // eastLng - max lng
-    region.latitude + region.latitudeDelta// northLat - max lat
+    region.longitude + region.longitudeDelta, // eastLng - max lng
+    region.latitude + region.latitudeDelta // northLat - max lat
   ];
 
   getBoundsZoomLevel = (bounds, mapDim) => {
@@ -111,7 +118,7 @@ export default class MapWithClustering extends Component {
     const ZOOM_MAX = 20;
 
     function latRad(lat) {
-      const sin = Math.sin(lat * Math.PI / 180);
+      const sin = Math.sin((lat * Math.PI) / 180);
       const radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
       return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
     }
@@ -122,7 +129,7 @@ export default class MapWithClustering extends Component {
 
     const latFraction = (latRad(bounds[3]) - latRad(bounds[1])) / Math.PI;
     const lngDiff = bounds[2] - bounds[0];
-    const lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+    const lngFraction = (lngDiff < 0 ? lngDiff + 360 : lngDiff) / 360;
     const latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
     const lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
 
@@ -134,27 +141,29 @@ export default class MapWithClustering extends Component {
 
     if (this.props.clustering && this.superCluster) {
       const bBox = this.calculateBBox(this.state.currentRegion);
-      let zoom = this.getBoundsZoomLevel(bBox, { height: h(100), width: w(100) });
+      const zoom = this.getBoundsZoomLevel(bBox, { height: h(100), width: w(100) });
       const clusters = await this.superCluster.getClusters([bBox[0], bBox[1], bBox[2], bBox[3]], zoom);
-      const CustomDefinedMarker = this.props.customDefinedMarker || CustomMarker
-      
-      clusteredMarkers = clusters.map(cluster => (<CustomDefinedMarker
-        pointCount={cluster.properties.point_count}
-        clusterId={cluster.properties.cluster_id}
-        geometry={cluster.geometry}
-        clusterStyle={this.state.clusterStyle}
-        clusterTextStyle={this.state.clusterTextStyle}
-        marker={cluster.properties.point_count === 0 ? cluster.marker : null}
-        key={JSON.stringify(cluster.geometry) + cluster.properties.cluster_id + cluster.properties.point_count}
-        onClusterPress={this.props.onClusterPress}
-      />));
+      const CustomDefinedMarker = this.props.customDefinedMarker || CustomMarker;
+
+      clusteredMarkers = clusters.map(cluster => (
+        <CustomDefinedMarker
+          pointCount={cluster.properties.point_count}
+          clusterId={cluster.properties.cluster_id}
+          geometry={cluster.geometry}
+          clusterStyle={this.state.clusterStyle}
+          clusterTextStyle={this.state.clusterTextStyle}
+          marker={cluster.properties.point_count === 0 ? cluster.marker : null}
+          key={JSON.stringify(cluster.geometry) + cluster.properties.cluster_id + cluster.properties.point_count}
+          onClusterPress={this.props.onClusterPress}
+        />
+      ));
     } else {
       clusteredMarkers = this.state.markers.map(marker => marker.marker);
     }
 
     this.setState({
       clusteredMarkers,
-      currentRegion,
+      currentRegion
     });
   };
 
@@ -172,8 +181,9 @@ export default class MapWithClustering extends Component {
     return (
       <MapView
         {...this.removeChildrenFromProps(this.props)}
-        ref={(ref) => { this.root = ref; }}
-        region={this.state.currentRegion}
+        ref={(ref) => {
+          this.root = ref;
+        }}
         onRegionChangeComplete={this.onRegionChangeComplete}
       >
         {this.state.clusteredMarkers}
@@ -184,7 +194,6 @@ export default class MapWithClustering extends Component {
 }
 
 MapWithClustering.propTypes = {
-  region: PropTypes.object,
   clustering: PropTypes.bool,
   radius: PropTypes.number,
   clusterColor: PropTypes.string,
@@ -192,10 +201,10 @@ MapWithClustering.propTypes = {
   clusterBorderColor: PropTypes.string,
   clusterBorderWidth: PropTypes.number,
   clusterTextSize: PropTypes.number,
-  onClusterPress: PropTypes.func,
+  onClusterPress: PropTypes.func
 };
 
-const totalSize = num => (Math.sqrt((h(100) * h(100)) + (w(100) * w(100))) * num) / 100;
+const totalSize = num => (Math.sqrt(h(100) * h(100) + w(100) * w(100)) * num) / 100;
 
 MapWithClustering.defaultProps = {
   clustering: true,
@@ -205,5 +214,5 @@ MapWithClustering.defaultProps = {
   clusterBorderColor: '#FF5252',
   clusterBorderWidth: 1,
   clusterTextSize: totalSize(2.4),
-  onClusterPress: () => {},
+  onClusterPress: () => {}
 };
